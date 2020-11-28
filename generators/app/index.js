bbfe7084f28ef010fc614d7a825f8543b86c394b
hsk <935789701@qq.com>
2020-11-28 20:33:37 +0800

    feat: initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,42 @@
+const Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
+      }
+    ])
+    .then(answers => {
+      this.answers = answers
+    })
+  }
+
+  writing() {
+    // 把每个文件都通过模板转换到目标路径
+    const templates = [
+      '.gitignore',
+      'babel.config.js',
+      'package-lock.json',
+      'package.json',
+      'README.md',
+      'public/favicon.ico',
+      'public/index.html',
+      'src/App.vue',
+      'src/main.js',
+      'src/assets/logo.png',
+      'src/components/HelloWorld.vue'
+    ]
+
+    templates.forEach(item => {
+      this.fs.copyTpl(
+        this.templatePath(item),
+        this.destinationPath(item),
+        this.answers
+      )
+    })
+  }
+}
